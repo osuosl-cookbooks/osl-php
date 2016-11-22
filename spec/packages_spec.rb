12ff7ae1d63fa69c7255ccc05a2e10510230ebd5
fahlmant <fahlmantaylor@gmail.com>
2016-11-22 15:57:30 -0800

    packages spec should use packages recipe

--- spec/packages_spec.rb
+++ spec/packages_spec.rb
@@ -1,10 +1,13 @@
 require_relative 'spec_helper'
 
-describe 'osl-php::apc' do
+describe 'osl-php::packages' do
   [CENTOS_7_OPTS, CENTOS_6_OPTS, DEBIAN_8_OPTS].each do |pltfrm|
     context "on #{pltfrm[:platform]} #{pltfrm[:version]}" do
       cached(:chef_run) do
         ChefSpec::SoloRunner.new(pltfrm).converge(described_recipe)
+      end
+      before do
+        chef_run.node.set['osl-php']['packages'] = 'php-gd'
       end
       it 'converges successfully' do
         expect { chef_run }.to_not raise_error
