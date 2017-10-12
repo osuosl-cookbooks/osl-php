fb72e4cf2f18ef642cb5accfc3ec1b05f487a097
Travis Whitehead <whitehet@osuosl.org>
2017-10-12 14:27:09 -0700

    Update test attributes so php::default doesn't fail.

--- spec/packages_spec.rb
+++ spec/packages_spec.rb
@@ -5,14 +5,20 @@ describe 'osl-php::packages' do
     context "on #{pltfrm[:platform]} #{pltfrm[:version]}" do
       cached(:chef_run) do
         ChefSpec::SoloRunner.new(pltfrm) do |node|
-          node.set['osl-php']['packages'] = %w(php-fpm php-gd)
+          node.set['osl-php']['packages'] = %w(php
+                                               php-devel
+                                               php-fpm
+                                               php-gd
+                                               php-pear)
         end.converge(described_recipe)
       end
       it 'converges successfully' do
         expect { chef_run }.to_not raise_error
       end
       it do
-        expect(chef_run).to install_package(%w(php-fpm php-gd))
+        expect(chef_run).to install_package(
+          chef_run.node['osl-php']['packages']
+        )
       end
     end
   end
