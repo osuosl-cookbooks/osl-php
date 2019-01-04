c3f4b966118674e7a6c931a95f37313d05d69d84
Hunter Lannon <kindlehl@osusol.org>
2019-01-04 09:48:53 -0800

    brought test cookbook back to life

---
+++ test/cookbooks/php_test/metadata.rb
@@ -0,0 +1,14 @@
+name             'php_test'
+maintainer       'Oregon State University'
+maintainer_email 'chef@osuosl.org'
+license          'Apache-2.0'
+chef_version     '>= 12.18' if respond_to?(:chef_version)
+issues_url       'https://github.com/osuosl-cookbooks/php_ini-test/issues'
+source_url       'https://github.com/osuosl-cookbooks/php_ini-test'
+description      'Installs/Configures php_ini-test'
+long_description 'Installs/Configures php_ini-test'
+version          '0.1.0'
+
+depends          'osl-php'
+
+supports         'centos', '~> 7.0'
---
+++ test/cookbooks/php_test/recipes/php_ini.rb
@@ -0,0 +1,129 @@
+#
+# Cookbook:: php_ini-test
+# Recipe:: default
+#
+# Copyright:: 2019, Oregon State University
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); # you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+with_sections = {
+  'allow_call_time_pass_reference' => 'Off',
+  'allow_url_fopen' => 'On',
+  'asp_tags' => 'Off',
+  'browscap' => {
+    'dbx.colnames_case' => '"lowercase"',
+    'default_mimetype' => '"text/html"',
+    'display_errors' => 'Off',
+    'display_startup_errors' => 'Off',
+  },
+  'Informix' => {
+    'ingres.allow_persistent' => 'On',
+  },
+  'Ingres II' => {
+    'ingres.max_links' => '-1',
+    'ingres.max_persistent' => '-1',
+    'log_errors_max_len' => '1024',
+    'log_errors' => 'On',
+  },
+  'MSSQL' => {
+    'mssql.allow_persistent' => 'On',
+    'mssql.compatability_mode' => 'Off',
+    'mssql.secure_connection' => 'Off',
+  },
+  'MySQL' => {
+    'mysql.allow_persistent' => 'On',
+    'mysql.connect_timeout' => '60',
+  },
+  'MySQLI' => {
+    'mysqli.reconnect' => 'Off',
+    'mysql.max_links' => '-1',
+    'mysql.max_persistent' => '-1',
+    'mysql.trace_mode' => 'Off',
+  },
+  'ODBC' => {
+    'odbc.defaultbinmode' => '1',
+    'odbc.defaultlrl' => '4096',
+    'odbc.max_links' => '-1',
+    'odbc.max_persistent' => '-1',
+  },
+  'PostgresSQL' => {
+    'post_max_size' => '8M',
+    'precision' => '14',
+    'register_argc_argv' => 'On',
+  },
+  'Session' => {
+    'session.auto_start' => '0',
+    'session.bug_compat_42' => '0',
+    'session.bug_compat_warn' => '1',
+    'session.cache_expire' => '180',
+  },
+}
+
+no_sections = {
+  'allow_call_time_pass_reference' => 'Off',
+  'allow_url_fopen' => 'On',
+  'asp_tags' => 'Off',
+  'dbx.colnames_case' => '"lowercase"',
+  'default_mimetype' => '"text/html"',
+  'display_errors' => 'Off',
+  'display_startup_errors' => 'Off',
+  'ingres.allow_persistent' => 'On',
+  'ingres.max_links' => '-1',
+  'ingres.max_persistent' => '-1',
+  'log_errors_max_len' => '1024',
+  'log_errors' => 'On',
+  'mssql.allow_persistent' => 'On',
+  'mssql.compatability_mode' => 'Off',
+  'mssql.secure_connection' => 'Off',
+  'mysql.allow_persistent' => 'On',
+  'mysql.connect_timeout' => '60',
+  'mysqli.reconnect' => 'Off',
+  'mysql.max_links' => '-1',
+  'mysql.max_persistent' => '-1',
+  'mysql.trace_mode' => 'Off',
+  'odbc.defaultbinmode' => '1',
+  'odbc.defaultlrl' => '4096',
+  'odbc.max_links' => '-1',
+  'odbc.max_persistent' => '-1',
+  'post_max_size' => '8M',
+  'precision' => '14',
+  'register_argc_argv' => 'On',
+  'session.auto_start' => '0',
+  'session.bug_compat_42' => '0',
+  'session.bug_compat_warn' => '1',
+  'session.cache_expire' => '180',
+}
+
+php_ini 'with_sections_rendered' do
+  options with_sections
+end
+
+php_ini 'no_sections_rendered' do
+  options no_sections
+end
+
+php_ini 'no_sections_rendered_removed' do
+  options no_sections
+end
+
+php_ini 'no_sections_rendered_removed' do
+  options no_sections
+  action :remove
+end
+
+cookbook_file '/etc/php.d/with_sections_static' do
+  source 'with_sections_static'
+end
+
+cookbook_file '/etc/php.d/no_sections_static' do
+  source 'no_sections_static'
+end
