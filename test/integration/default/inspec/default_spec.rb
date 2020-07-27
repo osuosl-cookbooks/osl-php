c8df406c059e4542a0a097e004bb63b5ecc0c350
Robert Detjens <detjensrobert@osuosl.org>
2020-07-27 16:41:35 -0700

    remove unneeded arg from phpshow/phpcheck kitchen test

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -26,12 +26,12 @@ describe command('/usr/local/bin/phpshow -h') do
   its('stdout') { should match 'phpcheck / phpshow help:' }
 end
 
-describe command('/usr/local/bin/phpcheck -d /var/www test for me') do
+describe command('/usr/local/bin/phpcheck test for me') do
   its('stdout') { should match /^phphelpertest$/ }
   its('stdout') { should match /^test_the_second$/ }
 end
 
-describe command('/usr/local/bin/phpshow -d /var/www test for me') do
+describe command('/usr/local/bin/phpshow test for me') do
   its('stdout') { should match %r{phphelpertest/test_file.php.+test for me!} }
   its('stdout') { should match %r{test_the_second/me_too.php.+test for me too!} }
 end
