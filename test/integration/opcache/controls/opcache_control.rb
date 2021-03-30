a191e9995fbf4ae2997f889745bf44c061d9ee56
Ryan Zimmerman <zimmerry@oregonstate.edu>
2021-03-30 13:42:21 -0700

    Condense opcache profiles into one

--- test/integration/opcache/controls/opcache_control.rb
+++ test/integration/opcache/controls/opcache_control.rb
@@ -1,12 +1,19 @@
 # Include selinux tests
 include_controls 'selinux'
 
-control 'opcache-php72_config' do
+version = input('version')
+
+control 'opcache' do
   title 'Verify opcache package is installed and configured'
 
   ['echo "<?php phpinfo(); ?>" | php', 'curl localhost'].each do |cmd|
     describe command cmd do
-      its(:stdout) { should match /PHP Version.*7.2/ }
+      case version
+      when '5.6'
+        its(:stdout) { should match /PHP Version.*5.6/ }
+      when '7.2'
+        its(:stdout) { should match /PHP Version.*7.2/ }
+      end
       its(:stdout) { should match /opcache\.memory_consumption.+1024/ }
       its(:stdout) { should match /opcache\.max_accelerated_files.+1000/ }
       its(:stdout) { should match /opcache\.enable.+On/ }
@@ -15,14 +22,21 @@ control 'opcache-php72_config' do
     end
   end
 
-  if os.release.to_i >= 8
-    describe package 'php-opcache' do
+  case version
+  when '5.6'
+    describe package 'php56u-opcache' do
       it { should be_installed }
-      its('version') { should >= '7.2' }
     end
-  else
-    describe package 'php72u-opcache' do
-      it { should be_installed }
+  when '7.2'
+    if os.release.to_i >= 8
+      describe package 'php-opcache' do
+        it { should be_installed }
+        its('version') { should >= '7.2' }
+      end
+    else
+      describe package 'php72u-opcache' do
+        it { should be_installed }
+      end
     end
   end
 end
