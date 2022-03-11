f8dbc742a3110d7f54f7749e18c0fd7b423a3703
Robert Detjens <detjensrobert@osuosl.org>
2022-03-11 15:47:00 -0800

    update spec tests for composer 3
    Signed-off-by: Robert Detjens <detjensrobert@osuosl.org>

--- spec/composer_spec.rb
+++ spec/composer_spec.rb
@@ -6,28 +6,37 @@ describe 'osl-php::composer' do
       cached(:chef_run) do
         ChefSpec::SoloRunner.new(pltfrm).converge(described_recipe)
       end
-      it 'converges successfully' do
+
+      before do
+        stub_command('php --version').and_return('PHP 7.4.28')
         stub_command("php -m | grep 'Phar'").and_return(false)
+      end
+
+      it 'converges successfully' do
         expect { chef_run }.to_not raise_error
       end
+
       it do
         expect(chef_run.node['composer']['url']).to eq('https://getcomposer.org/download/1.10.7/composer.phar')
       end
+
       %w(php::default composer::default osl-selinux::default).each do |r|
         it do
           expect(chef_run).to include_recipe(r)
         end
       end
+
       context 'install different version' do
         cached(:chef_run) do
           ChefSpec::SoloRunner.new(pltfrm) do |node|
             node.normal['osl-php']['composer_version'] = '1.2.1'
           end.converge(described_recipe)
         end
+
         it 'converges successfully' do
-          stub_command("php -m | grep 'Phar'").and_return(false)
           expect { chef_run }.to_not raise_error
         end
+
         it do
           expect(chef_run.node['composer']['url']).to eq('https://getcomposer.org/download/1.2.1/composer.phar')
         end
