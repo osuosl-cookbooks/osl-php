9b39b00030ae2fd50c2c5689e04e5415b20a9d71
Justin Fernbaugh <fernbaughj@gmail.com>
2023-04-14 11:22:33 -0700

    Added PHP 7.4 to ius archive versions

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,5 +3,5 @@ default['osl-php']['php_packages'] = []
 default['osl-php']['use_ius'] = false
 default['osl-php']['use_opcache'] = false
 # List PHP versions that require IUS archive repo: https://ius.io/LifeCycle/#php
-default['osl-php']['ius_archive_versions'] = %w(5.6 7.1 7.2)
+default['osl-php']['ius_archive_versions'] = %w(5.6 7.1 7.2 7.4)
 default['osl-php']['composer_version'] = '2.2.18'
