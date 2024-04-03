18d201bf21679711d0fcdd3e50881ba2a9744445
ArtofBugs <74070945+ArtofBugs@users.noreply.github.com>
2024-04-03 15:47:25 -0700

    Add ini spec test
    Signed-off-by: ArtofBugs <74070945+ArtofBugs@users.noreply.github.com>

---
+++ spec/unit/resources/ini_spec.rb
@@ -0,0 +1,73 @@
+require_relative '../../spec_helper'
+
+describe 'osl_php_ini' do
+  cached(:subject) { chef_run }
+  platform 'almalinux', '8'
+  step_into :osl_php_ini
+
+  recipe do
+    osl_php_ini 'default' do
+      options data: {'test' => 'test'}
+    end
+  end
+
+  it do
+    is_expected.to create_directory('/etc/php.d default').with(path: '/etc/php.d')
+    is_expected.to create_template('/etc/php.d/default.ini').with(
+      source: 'php_ini.erb',
+      cookbook: 'osl-php',
+      mode: '0644'
+    )
+  end
+
+  context 'CentOS 7' do
+    cached(:subject) { chef_run }
+    recipe do
+      osl_php_ini 'default'
+    end
+  end
+
+  context 'options' do
+    cached(:subject) { chef_run }
+
+    with_sections = {
+      'key' => 'value',
+      'section' => {
+        'section_key' => 'section_value',
+      },
+    }
+
+    no_sections = {
+      'key' => 'value',
+      'section_key' => 'section_value',
+    }
+
+    recipe do
+      osl_php_ini 'with_sections' do
+        options with_sections
+      end
+      osl_php_ini 'no_sections' do
+        options no_sections
+      end
+    end
+
+    it do
+      is_expected.to create_template('/etc/php.d/with_sections.ini').with(
+        variables: {:data => with_sections}
+      )
+    end
+  end
+
+  context 'Remove ini' do
+    cached(:subject) { chef_run }
+    recipe do
+      osl_php_ini 'remove' do
+        action :remove
+      end
+    end
+
+    it do
+      is_expected.to delete_file('/etc/php.d/remove.ini')
+    end
+  end
+end
